60680c46434689032a531f4a97e28d32613602d7
Rafael <rrodriguez@hoyosintegrity.com>
2019-07-24 17:18:14 -0400

    include all source files

---
+++ cryptopp/cryptopp.h
@@ -0,0 +1,13 @@
+//
+//  cryptopp.h
+//  cryptopp
+//
+//  Created by Rafael Rodriguez on 7/16/19.
+//  Copyright © 2019 Rafael Adames. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cryptopp : NSObject
+
+@end
---
+++ cryptopp/cryptopp.m
@@ -0,0 +1,15 @@
+//
+//  cryptopp.m
+//  cryptopp
+//
+//  Created by Rafael Rodriguez on 7/16/19.
+//  Copyright © 2019 Rafael Adames. All rights reserved.
+//
+
+#import "cryptopp.h"
+
+
+
+@implementation cryptopp
+
+@end
